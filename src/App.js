be731b5243f1f75efb530ad2c84ee2c5436ae24f
kesavamuthu <kesavamuthu77@gmail.com>
2020-08-17 10:20:59 +0530

    remined

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
 
 import "./App.css";
-import Init from "./components/init";
+import Init from "./components/index";
 import config from "./config";
 import Avatar from "./components/Avatar";
-import CreateSurvey from "./components/CreateSurvey";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import TakeSurvey from "./components/TakeSurvey";
+import CreateSurvey from "./components/createSurvey/CreateSurvey";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import TakeSurvey from "./components/takeSurvey/TakeSurvey";
 
 function App() {
   return (
     <div className="App" style={{ position: "relative", top: "2vh" }}>
+      {/* <Link to="/"> */}
       <Avatar url={config.imgSrc} />
+      {/* </Link> */}
       <Router>
         <Switch>
           <Route exact path="/">
