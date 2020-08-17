a34f067ee58e45cab51a3ca9d2bb6c1238524938
kesavamuthu <kesavamuthu77@gmail.com>
2020-08-17 12:04:46 +0530

    build

---
+++ build/precache-manifest.5f29bb5c82d969f06ff781910fdbfa05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cba3e906ed8a4efde640f203983debcc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45441d8df8adec7d245b",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ec7293d3bc5351a774be",
+    "url": "/static/css/main.dfa1e072.chunk.css"
+  },
+  {
+    "revision": "45441d8df8adec7d245b",
+    "url": "/static/js/2.6180208d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6180208d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec7293d3bc5351a774be",
+    "url": "/static/js/main.83ca02f5.chunk.js"
+  },
+  {
+    "revision": "6eedce75ce81024009bf",
+    "url": "/static/js/runtime-main.44e50aa0.js"
+  }
+]);
--- src/config.js
+++ src/config.js
@@ -1,5 +1,8 @@
 let config = {
-  url: "http://localhost:3000/",
+  url:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000/"
+      : "http://18.221.148.171:3000/",
   imgSrc:
     "https://banner2.cleanpng.com/20180418/izw/kisspng-white-tiger-willard-claw-clip-art-wrestling-5ad76518567760.0786027715240655603542.jpg",
 };
